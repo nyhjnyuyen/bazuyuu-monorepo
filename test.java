1e4949c5b014848eadeddfe0f8f958c84598b183
nyhjnyuyen <ntn14204@uga.edu>
2025-10-19 18:33:37 -0400

    Initial commit

---
+++ test.java
@@ -0,0 +1,8 @@
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(new BCryptPasswordEncoder().encode("admin123"));
+    }
+
+}
