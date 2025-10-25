95a3bae6965872d3bb104846e612e4482dbc7de8
nyhjnyuyen <ntn14204@uga.edu>
2025-10-25 00:21:28 -0400

    change id from long to string

--- backend/src/main/java/com/example/bazuuyu/entity/Product.java
+++ backend/src/main/java/com/example/bazuuyu/entity/Product.java
@@ -17,7 +17,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
 
     private String name;
     private String description;
